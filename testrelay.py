223a270fd44e17ee789385cfeba61d29b365ca58
Tom Nguyen <huuthong103@gmail.com>
2018-04-24 21:52:28 +0000

    update relay for light

--- testrelay.py
+++ testrelay.py
@@ -5,24 +5,18 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 # Set relay pins as output
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
 
 while (True):
     
     # Turn all relays ON
-    GPIO.output(18, GPIO.HIGH)
-    GPIO.output(23, GPIO.HIGH)
-    GPIO.output(24, GPIO.HIGH)
-    GPIO.output(25, GPIO.HIGH)
+    GPIO.output(16, GPIO.HIGH)
+
     # Sleep for 5 seconds
     sleep(5) 
     # Turn all relays OFF
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(23, GPIO.LOW)
-    GPIO.output(24, GPIO.LOW)
-    GPIO.output(25, GPIO.LOW)   
+    GPIO.output(16, GPIO.LOW)
+
     # Sleep for 5 seconds
     sleep(5)
